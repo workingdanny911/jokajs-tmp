372e791596e348731ba92eb4e7e7b456fbbec328
workingdanny <asds8902@gmail.com>
2022-07-04 13:02:58 +0900

    privileges.sql -> mdb-privileges.sql

--- database/message_handlers.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE IF NOT EXISTS message_handlers (
-    id UUID,
-    handler_name VARCHAR(100),
-    UNIQUE(id, handler_name)
-)
--- database/messaging/mdb-privileges.sql
+++ database/messaging/mdb-privileges.sql
@@ -1,2 +1,4 @@
+# make sure we can truncate the messages table during testing
+
 ALTER SCHEMA message_store OWNER TO message_store;
 ALTER DEFAULT PRIVILEGES IN SCHEMA message_store GRANT UPDATE, INSERT, SELECT, DELETE ON TABLES TO message_store;
